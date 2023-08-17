ca0a7f9b6b4a33a65d84280b2e2bcf8b48dae9e6
Anand Lilhare <anandlilhare69@gmail.com>
2023-08-17 12:19:40 +0530

    updating the code

--- app/src/main/java/com/example/unitconverterapp/MainActivity.kt
+++ app/src/main/java/com/example/unitconverterapp/MainActivity.kt
@@ -10,11 +10,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.unitconverterapp.compose.BaseScreen
+import com.example.unitconverterapp.data.ConverterDatabase
+import com.example.unitconverterapp.data.ConverterRepositoryImpl
 import com.example.unitconverterapp.ui.theme.UnitConverterAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val dao = ConverterDatabase.getInstance(application).converterDAO
+        val repository = ConverterRepositoryImpl(dao)
+        val factory = ConverterViewModelFactory(repository)
+
         setContent {
             UnitConverterAppTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BaseScreen(factory = factory)
                 }
             }
         }
